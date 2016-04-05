4bbfa22c184b7d3c3a0d5a7a9b516ccdf2300cdc
陈朗 <chenlang@rongcloud.cn>
2016-04-05 13:32:25 +0800

    test2分支中添加内容

--- src/lang/Main.java
+++ src/lang/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello git from branch test!");
+		
+		System.out.println("hello git from branch test2!");
 	}
 
 }
