b7d82d4e7dbb353f38281284b6ee4fb30a50d7e2
陈朗 <chenlang@rongcloud.cn>
2016-04-05 13:31:23 +0800

    调整test1分支打印内容

--- src/lang/Main.java
+++ src/lang/Main.java
@@ -3,7 +3,7 @@ package lang;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello git from branch test!");
+		System.out.println("hello git from branch test1!");
 	}
 
 }
