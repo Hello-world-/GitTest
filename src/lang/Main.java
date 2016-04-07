ccb414193516ad51b32ad2bbddc08ac7683c1e0c
陈朗 <1989.chenlang@163.com>
2016-04-07 10:11:57 +0800

    添加emoji相关测试内容 😄😄

--- src/lang/Main.java
+++ src/lang/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("hello git from branch test1!");
+		
+		System.out.println("test emoji :   😄😄\n😄😄\r😄😄");
 
 	}
 
